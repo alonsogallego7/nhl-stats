e17f28a66f2c1ba3a6a944100a7da700c0606f09
alonsogallego7 <alonso.galpac@educa.jcyl.es>
2026-04-25 19:28:20 +0200

    update cors

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://nhl-stats-server.vercel.app/' //http://localhost:3000
 };
--- server/app.js
+++ server/app.js
@@ -11,7 +11,11 @@ var statsRouter = require('./routes/stats');
 
 var app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://nhl-stats-blush.vercel.app', // Tu URL de Angular
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
